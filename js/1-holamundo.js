5da3bf9c34ac5fb7a8ecbef895d5e0431b2929ed
Emilse <emilsearias@gmail.com>
2021-09-27 11:05:17 -0300

    introduccion de javascript

---
+++ js/1-holamundo.js
@@ -0,0 +1,40 @@
+//Esto es un comentario simple
+
+/* Esto es un
+comentario de
+multiples lineas
+*/
+
+// mostrar un mensaje en la pantalla
+document.write('Hola mundo');
+
+// mostrar un mensaje en la consola
+console.log("hola mundo");
+
+// mostrar un mensaje en una ventana emergente
+//  alert('Hola comision 18i');
+
+// crear o declarar o inicializar una variable (var - let - const)
+
+let persona = 'Axel Campo';
+
+document.write('<br>Persona: '+ persona);
+
+// modificar el valor de una variable
+persona = 'Agus lorenzetti';
+
+document.write('<br>Persona: '+ persona);
+
+let edad = 23;
+
+document.write('<br>Edad: '+ edad);
+
+// crear variables constantes
+const URL = 'https://campus.rollingcodeschool.com/';
+
+document.write('<br>Link: '+ URL);
+
+
+
+
+
