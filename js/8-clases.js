cd34720f6b602cbb1022a60bec024ff7048fbc53
Emilse <emilsearias@gmail.com>
2021-10-25 11:34:13 -0300

    se agrego un ejemplo de como agregar nuestros objetos a un bucle los get y set, comenzamos agregar la clase alumno

--- js/8-clases.js
+++ js/8-clases.js
@@ -30,7 +30,44 @@ class Persona{
     mostrarResumen(){
         
     }
+
+    // crear las propiedades conmutadas get y set
+    // muestran una propiedad
+    get mostrarNombre(){
+        return this.nombre;
+    }
+
+    get mostrarApellido(){
+        return this.apellido;
+    }
+// modifican una propiedad
+    set modificarNombre(nuevoNombre){
+        this.nombre = nuevoNombre;
+    }
+
+    set modificarApellido(nuevoApellido){
+        this.apellido = nuevoApellido;
+    }
+
 }
+
+// herencia
+class Alumno extends Persona {
+    // primer metodo de una clase
+    constructor(apellido, paramNombre, email, edad, direccion, fotoPerfil, dni, curso, legajo){
+    //    invoco al constructor de la clase persona
+        super(paramNombre,apellido,email, edad, direccion, fotoPerfil, dni);
+        // agregar las nuevas propiedades
+        this.curso = curso;
+        this.legajo = legajo;
+        this.calificaciones = [];
+    }
+}
+
+
+// declarar variables
+// crear el arreglo
+let listaPersonas = [];
 
 // como crear un objeto, crear instanciar
 let luciana = new Persona('Luciana','Medina','lmedina@gmail.com', 25, 'aqui va la direccion','url', 39888999);
@@ -38,6 +75,26 @@ console.log(luciana);
 
 let axel = new Persona('Axel', 'Campo','acampo@gmail.com', 23,`una direccion aleatoria`,'otra url', 39666777);
 
+// guardar los objetos dentro del arreglo
+listaPersonas.push(luciana);
+listaPersonas.push(axel);
+
+console.log(listaPersonas);
 // usar los metodos de la clase persona
-luciana.mostrarDatos();
-axel.mostrarDatos();
+
+// luciana.mostrarDatos();
+// axel.mostrarDatos();
+
+for(let i =0; i< listaPersonas.length; i++){
+    console.log(listaPersonas[i])
+    listaPersonas[i].mostrarDatos();
+}
+
+// modificar una propiedad del objeto
+// luciana.direccion = 'nueva direccion'; mala practica
+
+document.write(`<h6>Apellido: ${axel.mostrarNombre} ${axel.mostrarApellido} </h6>`)
+
+luciana.modificarNombre = 'Lu';
+
+document.write(`<h6>Apellido: ${luciana.mostrarNombre} ${luciana.mostrarApellido} </h6>`)
